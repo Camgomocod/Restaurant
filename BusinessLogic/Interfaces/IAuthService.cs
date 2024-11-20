b06c42023fd08f9070894263720f61d841d2dd03
Chancar <107017249+Camgomocod@users.noreply.github.com>
2024-11-20 11:44:15 -0500

    Definición de clases para la logica de negocio para cada una de las secciones de control de usuario, al igual de la implementación de las interfaces necesarias para cada una de las clases que maneja la lógica de negocios, tambíen se definio estilos para ser usadas dentro de la interfaz de administrador. Queda pendiente la implementación del DataTemplate, no estoy muy seguro de como implementarlo.

---
+++ BusinessLogic/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.BusinessLogic.Interfaces
+{
+    public interface IAuthService
+    {
+        // Implementar la funcionalidad de hacer la distinción entre usuarios 
+        bool Login(string username, string password);
+        void Logout();
+        bool IsAuthenticade();
+    }
+}
---
+++ BusinessLogic/Services/Login/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using Restaurant.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.BusinessLogic.Services.Login
+{
+    internal class AuthService : IAuthService
+    {
+        public bool IsAuthenticade()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
