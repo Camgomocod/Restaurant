9eda8d852c54d89b4ca74b04aa1cd3817489378e
Chancar <107017249+Camgomocod@users.noreply.github.com>
2024-11-23 12:43:30 -0500

    Avance Interfaz de administrador.

--- BusinessLogic/Services/Admin/CustomerService.cs
+++ BusinessLogic/Services/Admin/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Restaurant.BusinessLogic.Services
 {
-    internal class CustomerService
+    internal class CustomerService 
     {
+        // Esta pendiente hacer la conexión con el data acces para obtener esos datos y ser usados aqui
+        public int CustomerID
+        {
+            get { return}
+        }
     }
 }
